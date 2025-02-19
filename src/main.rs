02f94a08af8ad028b514d6cbd48c0ac054428ee6
jflessau <jan.jf@me.com>
2025-02-19 20:38:00 +0100

    privacy: rm google fonts

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,6 @@ pub fn App() -> impl IntoView {
         <Title text="Ritmo" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-            rel="stylesheet"
-        />
-
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
